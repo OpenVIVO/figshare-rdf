46e7a887b3699353bd9699709f3c838bc6d72b9b
Ted Lawless <lawlesst@gmail.com>
2016-04-07 20:39:26 -0400

    Add basic tests and vcr fixtures.

---
+++ tests/test_figshare2vivo.py
@@ -0,0 +1,68 @@
+from unittest import TestCase
+import vcr
+
+from rdflib import URIRef, Literal
+
+from figshare2vivo import make_figshare_rdf, get_figshare_article, uri_prefix
+#namespace
+from figshare2vivo import RDF, RDFS, BIBO, VIVO
+
+my_vcr = vcr.VCR(
+    cassette_library_dir='fixtures',
+)
+
+from pprint import pprint
+
+
+class TestMappings(TestCase):
+    work = '3117808'
+    doi = '10.6084/m9.figshare.3117808.v2'
+
+    @my_vcr.use_cassette()
+    def test_get_article(self):
+        meta = get_figshare_article(self.work)
+        self.assertTrue(meta['citation'].startswith('Krafft, Dean; Conlon, Michael'))
+        self.assertEqual(meta['doi'], self.doi)
+        self.assertEqual(
+            meta['description'],
+            'Status of the VIVO Project -- research projects, sites, on-going c ollaborations'
+        )
+
+    @my_vcr.use_cassette()
+    def test_make_figshare_rdf(self):
+        meta = get_figshare_article(self.work)
+        g = make_figshare_rdf(meta)
+
+        print g.serialize(format='turtle')
+
+        created_uri = [u for u in g.subjects(RDF.type, BIBO.Slideshow)][0]
+
+        # uri
+        self.assertEqual(
+            URIRef(uri_prefix + self.work),
+            created_uri
+        )
+
+        # title
+        self.assertEqual(
+            g.value(created_uri, RDFS.label),
+            Literal('VIVO Status -- Duraspace Summit March 16, 2016')
+        )
+
+        # date
+        self.assertEqual(
+            g.value(created_uri, VIVO.publishedDate),
+            URIRef('http://openvivo.org/a/date2016-03-17')
+        )
+
+        # doi
+        created_doi = g.value(created_uri, BIBO.doi)
+        self.assertEqual(
+            created_doi,
+            Literal(self.doi)
+        )
+
+
+
+
+        #assert 1 == 2
